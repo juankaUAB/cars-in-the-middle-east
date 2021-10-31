cca31aecac294a70e4d7c0a4a73e72363b7876d0
juankaUAB <57271164+juankaUAB@users.noreply.github.com>
2021-10-31 22:34:15 +0100

    mineria de dades 2

--- Code/DataMining.py
+++ Code/DataMining.py
@@ -6,20 +6,22 @@ import scipy
 import seaborn as sns
 
 dataset = pd.read_csv("../BD/dataframe_YesIndex_YesHeader_C.csv")
+dataset = dataset.drop(columns=["Unnamed: 0"])
+dataset = dataset.drop_duplicates()
 
 print("Hi han valors NaN a la base de dades? " + str(dataset.isnull().values.any()))
 
 '''Adaptar les dades (normalitzar)'''
 dataset1 = dataset.values
-idsx = list(range(22))
-idsx.pop(18)
+idsx = list(range(21))
+idsx.pop(17)
 dataset1 = dataset1[:,idsx]
 scaler = MinMaxScaler()
 scaler.fit(dataset1)
 dataset1 = scaler.transform(dataset1)
 
-idsx.pop(20)
-y = dataset1[:,18]
+idsx.pop(19)
+y = dataset1[:,17]
 x = dataset1[:,idsx]
 
 
@@ -33,4 +35,10 @@ for i in range(x.shape[1]):
         plt.plot(xi, density(xi))
         plt.savefig("../Grafiques/hist/" + str(i) + ".png")
         plt.clf()
+
+fig, ax = plt.subplots(figsize=(20,20))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(dataset.corr(), ax=ax, cmap=cmap, vmin=0, vmax=1, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
+plt.savefig("../Grafiques/heatmap/mapa-calor.png")
 
