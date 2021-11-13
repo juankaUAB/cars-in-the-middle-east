b138149a52e6dd9f56e9b2f94bed28a5343dcfbf
juankaUAB <57271164+juankaUAB@users.noreply.github.com>
2021-11-13 14:50:07 +0100

    comparativa models

---
+++ Code/ExhaustiveSearch.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import seaborn as sns
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
+
+dataset = pd.read_csv("../BD/dataframe_YesIndex_YesHeader_C.csv")
+dataset = dataset.drop(columns=["Unnamed: 0"])
+dataset = dataset.drop_duplicates()
+dataset = dataset[["Torque","Cylinders","Horsepower","Top Speed","Engine Capacity","price"]]
+
+'''Adaptar les dades (normalitzar)'''
+dataset1 = dataset.values
+
+x = dataset1[:,:5]
+y = dataset1[:,5]
+
+'''Dividim el conjunt en train i test'''
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
+
+
+'''COMENÇEM LA CERCA EXHAUSTIVA DELS MILLORS PARAMETRES'''
+models = [LinearRegression(), DecisionTreeRegressor()]
+nombres_models = ["Linear Regression", "Decision Tree"]
+parametres = [{'fit_intercept': [True, False], 'normalize': [True,False]},
+              {"splitter":["best","random"],
+            "max_depth" : [1,3,5,10,15,25,30,50],
+            "min_weight_fraction_leaf":[0.0,0.1,0.2,0.3,0.4,0.5],
+           "max_features":["auto","log2","sqrt",None],
+           "max_leaf_nodes":[None,10,20,50,80,100,250,500] }]
+
+
+
+resum = []
+for i, model in enumerate(models):
+    clf = GridSearchCV(estimator=model, param_grid=parametres[i], cv=3, verbose=2, n_jobs=-1)
+    resum.append(clf.fit(X_train, y_train))
+    print("----" + str(nombres_models[i]) + "----")
+    print("Els millors parametres" + str(resum[i].best_params_))
+    print("La millor score: " + str(resum[i].best_score_))
+    
+    
+'''CERCA EXHAUSTIVA PER EL RANDOM FOREST'''
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+random_tree = RandomForestRegressor()
+randomCV = RandomizedSearchCV(estimator = random_tree, param_distributions = random_grid, 
+                              n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+randomCV.fit(X_train, y_train)
+print("----Random Forest----")
+print("Els millors parametres: " + str(randomCV.best_params_))
+print("La millor score: " + str(randomCV.best_score_))
+
--- Code/Models.py
+++ Code/Models.py
@@ -1,16 +1,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
 import scipy
 import seaborn as sns
+import graphviz
 
 dataset = pd.read_csv("../BD/dataframe_YesIndex_YesHeader_C.csv")
 dataset = dataset.drop(columns=["Unnamed: 0"])
 dataset = dataset.drop_duplicates()
-dataset = dataset[["Engine Capacity","Cylinders","Horsepower","Torque","Top Speed","price"]]
+dataset = dataset[["Torque","Cylinders","Horsepower","Top Speed","Engine Capacity","price"]]
 
 '''Adaptar les dades (normalitzar)'''
 dataset1 = dataset.values
@@ -25,17 +29,60 @@ y = dataset1[:,5]
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
 '''Creem el model de regressió lineal'''
+print("----REGRESSIO LINEAL----")
 model = LinearRegression()
 model.fit(X_train, y_train)
 prediccions = model.predict(X_test)
 
-precisio = model.score(X_train, y_train)
-print(precisio)
+print("Score del model: " + str(model.score(X_test, y_test)))
 
 '''Visualitzem la regressió'''
 plt.figure()
 ax = plt.scatter(X_test[:,0], y_test)
 plt.plot(X_test, prediccions, 'r')
 plt.savefig("../demo-linregr.png")
+plt.clf()
+
+'''Calculem el MSE'''
+mse = mean_squared_error(y_test, prediccions)
+print("Error quadratic mitja: " + str(mse))
+
+'''Decision Tree Regressor'''
+decision_tree = DecisionTreeRegressor()
+decision_tree.fit(X_train, y_train)
+print("---- DECISION TREE----")
+print("Profunditat del arbre: " + str(decision_tree.get_depth()))
+print("Numero de fulles del arbre: " + str(decision_tree.get_n_leaves()))
+prediccions_arbre = decision_tree.predict(X_test)
+
+print("Score del model: " + str(decision_tree.score(X_test, y_test)))
+
+'''Calculem el MSE'''
+mse = mean_squared_error(y_test, prediccions_arbre)
+print("Error quadratic mitja: " + str(mse))
+
+'''Visualitzem els resultats'''
+ax = plt.scatter(X_test[:,0], y_test)
+plt.plot(X_test, prediccions_arbre, 'r')
+plt.savefig("../demo-decisiontree.png")
+plt.clf()
+
+dot_data = export_graphviz(decision_tree, out_file='../decision_tree.dot')
+
+'''Random Forest Regressor'''
+print("---- RANDOM FOREST REGRESSOR ----")
+random_tree = RandomForestRegressor()
+random_tree.fit(X_train, y_train)
+
+print("Score del model: " + str(random_tree.score(X_test, y_test)))
+prediccions_randtree = random_tree.predict(X_test)
+mse = mean_squared_error(y_test, prediccions_randtree) 
+print("Error quadratic mitja: " + str(mse))
+
+'''Visualitzem els resultats'''
+ax = plt.scatter(X_test[:,0], y_test)
+plt.plot(X_test, prediccions_randtree, 'r')
+plt.savefig("../demo-randomtree.png")
+plt.clf()
 
 
